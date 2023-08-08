fa05d0537992f9dde7383626fb96a8e8df96906d
CookedMelon <cc2414635846@163.com>
2023-08-09 00:40:06 +0800

    8.9

---
+++ Exercise5/install/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/cc/Workspace/AFLlearn/Exercise5/install/lib"
+XML2_LIBS="-lxml2 -lz     -lm -ldl"
+XML2_INCLUDEDIR="-I/home/cc/Workspace/AFLlearn/Exercise5/install/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
